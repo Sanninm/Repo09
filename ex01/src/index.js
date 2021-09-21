9cb42c45836d63cc108694a63bcba72252adc479
Sanninm <mudzelet.sanin@gmail.com>
2021-09-21 12:59:45 +0200

    Boolean Values

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line 
+function myTrueBoolean() {
+ return true;
+
+}
+
+console.log(myTrueBoolean());
+// Only change code abode this line
+module.exports = myTrueBoolean;
